a14a9b48d51aa434f01cd005703b066c9873944d
Kireeti-28 <97286047+Kireeti-28@users.noreply.github.com>
2023-08-15 12:23:09 +0530

    updated mod

--- server/routes.go
+++ server/routes.go
@@ -5,8 +5,8 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/kireeti-28/soul-tidings/pkg/auth"
-	"github.com/kireeti-28/soul-tidings/pkg/handlers"
+	"github.com/kireeti-28/moms-music/pkg/auth"
+	"github.com/kireeti-28/moms-music/pkg/handlers"
 )
 
 func routes() http.Handler {
